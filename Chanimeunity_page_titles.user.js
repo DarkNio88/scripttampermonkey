a62743cf4a3c39dfb43c9c963a6e0fb69c8d8cd1
DarkNio <sonic-xx@hotmail.it>
2024-04-26 14:09:56 +0200

    Create Chanimeunity_page_titles.user.js

---
+++ Chanimeunity_page_titles.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Change animeunity page titles
+// @namespace    https://github.com/DarkNio88/scripttampermonkey
+// @version      0.3
+// @description  Changes titles of animeunity pages
+// @author       DarkNio
+// @match        https://www.animeunity.to/*
+// @icon         https://www.animeunity.to/favicon-32x32.png
+// @updateURL    https://github.com/DarkNio88/scripttampermonkey/raw/main/Chanimeunity_page_titles.user.js
+// @downloadURL  https://github.com/DarkNio88/scripttampermonkey/raw/main/Chanimeunity_page_titles.user.js
+// @license      MIT
+// @grant        window.onurlchange
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function changePageTitle() {
+        document.title = document.title.replace("Streaming SUB ITA/ITA & Download","").replace("AnimeUnity ~ ","");
+    }
+    if (window.onurlchange === null) {
+        window.addEventListener('urlchange', changePageTitle);
+    }
+    changePageTitle();
+})();
